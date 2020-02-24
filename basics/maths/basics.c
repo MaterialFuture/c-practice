a4cdfd4ced5f1634636c9d9dcb8202003eabc6d4
Konstantine <konstantinev@protonmail.com>
2020-02-24 17:54:20 -0600

    updated basic math script to include arrays and such

--- basics/maths/basics.c
+++ basics/maths/basics.c
@@ -3,10 +3,11 @@
 
 char std_name[15];
 /* Should the grades be set as floats, if there would ever be any floating point values then maybe */
-float test_grade_1;
-float test_grade_2;
-float grade_avg; /* The total ammount of the grades. */
+float grades[2];
+float grade_total = 0; /* The sum of grades. */
+float grade_avg;   /* The l ammount of the grades. */
 char grade_a;
+int i;
 
 char grade_check(float grade) {
     grade_a = 'A';
@@ -20,11 +21,21 @@ char grade_check(float grade) {
 int main() {
     strcpy(std_name, "Jim");                        // Set the kid's name
     printf("Hello %s, welcome to school...\n", std_name); // %s accepts a string into the string interpolation, %d would be for ints
-    test_grade_1 = 96;
-    test_grade_2 = 73;
+
+    /* Setup Grades */
+    grades[0] = 96.0;
+    grades[1] = 73.0;
+
+    /* Calculate grades */
     printf("Calculating grades...\n");
-    grade_avg = test_grade_1 + test_grade_2;     //Calculate grades
-    printf("Your Average: %f\n", grade_avg / 2);    //Print grade average, note the string interpolation
+    for(i=0; i<2; i++){
+        printf("%f, ", grades[i]);
+        grade_total = grade_total + grades[i];
+    }
+    printf("\nGrade Total: %f\n", grade_total);
+    grade_avg = grade_total / 2;
+    /* Print Grades */
+    printf("Your Average: %f\n", grade_avg);    //Print grade average, note the string interpolation
     printf("Your letter grade: %c\n", grade_check(grade_avg));
     printf("You're awesome, keep up the good work! \n");
     return(0);
