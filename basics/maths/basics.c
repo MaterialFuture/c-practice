00f2ed6b44c37ba5dfdbd6fa60440458cb7f38b6
Konstantine <konstantinev@protonmail.com>
2020-02-25 01:53:25 -0600

    updated grading script, works as intended now

--- basics/maths/basics.c
+++ basics/maths/basics.c
@@ -1,42 +1,54 @@
+/************************************************************************************/
+/* Title: Grades                                                                    */
+/* Description: Getting grades of students and print the information to the console */
+/************************************************************************************/
 #include <stdio.h>
 #include <string.h>
 
-char std_name[15];
-/* Should the grades be set as floats, if there would ever be any floating point values then maybe */
-float grades[2];
-float grade_total = 0; /* The sum of grades. */
-float grade_avg;   /* The l ammount of the grades. */
-char grade_a;
+char std_name[20];       /* Student Name */
+int grades[] = {96, 73}; /* Add grades here */
+int grade_amount;        /* Ammount of items in array */
+int grade_total = 0;     /* The sum of grades. */
+float grade_avg;         /* The l ammount of the grades. */
+char ltr_grade[] = { 'A', 'B', 'C', 'D', 'F'}; /* Letter Grades stored as an array */
 int i;
 
-char grade_check(float grade) {
-    grade_a = 'A';
+char grade_check(float grade) { /* A function to convert grade to letter grade */
     if(grade > 90.0) {
-        return grade_a;
+        return ltr_grade[0];    /* Letter Grades accessed via the array */
+    } else if(grade > 80.00) {
+        return ltr_grade[1];
+    } else if(grade > 70.00) {
+        return ltr_grade[2];
+    } else if(grade > 60.00) {
+        return ltr_grade[3];
     } else {
-        return 'F';
+        return ltr_grade[4];    /* Falback is an 'F' */
     }
 }
 
 int main() {
-    strcpy(std_name, "Jim");                        // Set the kid's name
-    printf("Hello %s, welcome to school...\n", std_name); // %s accepts a string into the string interpolation, %d would be for ints
+    printf("Enter Name: ");
+    fgets(std_name, sizeof(std_name), stdin);
+    std_name[strlen(std_name)-1] = '\0'; /* Removes newline at the end of string/char array */
 
-    /* Setup Grades */
-    grades[0] = 96.0;
-    grades[1] = 73.0;
+    /* strcpy(std_name, "Jim");                        // Set the kid's name */
+    printf("Pulling information for student, %s...\n", std_name); // %s accepts a string into the string interpolation, %d would be for ints
 
     /* Calculate grades */
     printf("Calculating grades...\n");
-    for(i=0; i<2; i++){
-        printf("%f, ", grades[i]);
+    grade_amount = sizeof(grades) / sizeof(int); //Get how many are in array by getting byte value array and dividing by size of int
+
+    for(i=0; i<grade_amount; i++){
+        printf("%d ", grades[i]);
         grade_total = grade_total + grades[i];
     }
-    printf("\nGrade Total: %f\n", grade_total);
-    grade_avg = grade_total / 2;
+    printf("\nGrade Total: %d\n", grade_total);
+    printf("Number Of Grades: %d\n", grade_amount);
+    grade_avg = grade_total / grade_amount;
     /* Print Grades */
     printf("Your Average: %f\n", grade_avg);    //Print grade average, note the string interpolation
     printf("Your letter grade: %c\n", grade_check(grade_avg));
-    printf("You're awesome, keep up the good work! \n");
+    printf("Keep up the good work %s\n", std_name);
     return(0);
 }
