d87edc7696d8ccd382bff2d4888600b0680ae0bf
Konstantine <konstantinev@protonmail.com>
2020-02-27 21:12:14 -0600

    created a very simple calculator

---
+++ basics/calc/calc.c
@@ -0,0 +1,52 @@
+/**************************************************/
+/* Title: Four Function Calculator                */
+/* Description  A simple four Function Calculator */
+/**************************************************/
+#include <stdio.h>
+
+
+char operator;   /* Maths operators such as +, -, *, and / stored as char */
+char input[100]; /* The user input, up to 100 char in the char array */
+int value;       /* The value you wish to do whatever with */
+int result = 0;  /* The value stored before/after calculation */
+
+int main() {
+    while (1) {
+        printf("Result: %d\n", result);            /* Print the result */
+        printf("Enter Operator and Number: ");     /* Prompt User to enter operator and value */
+        fgets(input, sizeof(input), stdin);        /* Get user input */
+        sscanf(input, "%c %d", &operator, &value); /* Scan input and store in appropriate variables */
+
+        if ((operator == 'q') || (operator == 'Q')) { /* q =quit */
+            printf("Quiting...\n");
+            break;
+        } else if (operator == '+') { /* Adds */
+            result += value;
+        } else if (operator == '-') { /* Subtract */
+            result -= value;
+        } else if (operator == '*') { /* Multiply */
+            result *= value;
+        } else if (operator == '/') { /* Divide */
+            if (value == 0) { /* If divide by zero then print error */
+                printf("Error: Cannot Divide by Zero, sorry bud \n");
+            } else {          /* If not zero then go ahead */
+                result /= value;
+            }
+        } else { /* If operator isn't one of four specified above then print error */
+            printf("Unknown operator: %c\n", operator);
+        }
+
+    /* Turn into Switch case for better performance on memory imo */
+    /*     switch (operator) { */
+    /*         case '+': */
+    /*             result += value; */
+    /*         case '-': */
+    /*             result -= value; */
+    /*         case '*': */
+    /*             result += value; */
+    /*         case '/': */
+    /*             result /= value; */
+    /*     } */
+
+    }
+}
