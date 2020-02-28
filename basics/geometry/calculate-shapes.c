4a44ed1015ad7c81aaa5d7f37f2706aeeea382e4
Konstantine <konstantinev@protonmail.com>
2020-02-27 19:45:51 -0600

    reorganization of directories

--- basics/geometry/calculate-shapes.c
+++ basics/geometry/calculate-shapes.c
@@ -5,6 +5,8 @@
 #include <stdio.h>
 
 const float PI = 3.1415927; /* Next step, create a class or script that calculates PI, then reference that here */
+float x = 12.5;
+float y = 15.3342;
 
 double square_calc(float h, float w) { /* calculate quadrilaterals like squares and rectangles. */
     return (h * w);
@@ -19,15 +21,24 @@ double sphere_calc(float r) {
     return (((4/3)* PI) * (r * r * r));
 }
 
+double square_per(float h, float w) { /* Calculates perimeter */
+    return (2 * (h + w));
+}
+
 int main(){
+    printf("Prints values based on \nX: %f\nY: %f\n\n", x, y);
+
+    printf("Calulate the perimeter of a squares and such...\n");
+    printf("Answer: %f \n", square_per(x, y));
+
     printf("Calulate the area of a squares and such...\n");
-    printf("Answer: %f \n", square_calc(12.5, 15.3443));
+    printf("Answer: %f \n", square_calc(x, y));
 
     printf("Calulate the area of a triangle...\n");
-    printf("Answer: %f \n", triangle_calc(12.5, 15.3443));
+    printf("Answer: %f \n", triangle_calc(x, y));
 
-    printf("Calulate the volume of a sphere...\n");
-    printf("Answer: %f \n", sphere_calc(12));
+    printf("Calulate the volume of a sphere, the radius being %f...\n", x);
+    printf("Answer: %f \n", sphere_calc(x));
 
-    /* return(0); */
+    return(0);
 }
