75c6c4ba7de1a1d2bb127b2713d4553d6288b8ac
Konstantine <konstantinev@protonmail.com>
2020-03-01 21:47:34 -0600

    Added recursion and cleaned up stuff

--- basics/grades/grades.c
+++ basics/grades/grades.c
@@ -6,7 +6,7 @@
 #include <string.h>
 
 char  std_name[20];        /* Student Name */
-int   grades[] = {96, 73}; /* Add grades here */
+int   grades[] = {96, 73, 100, 97, 78}; /* Add grades here */
 int   grade_amount;        /* Ammount of items in array */
 int   grade_total = 0;     /* The sum of grades. */
 float grade_avg;           /* The l ammount of the grades. */
@@ -14,7 +14,21 @@ int   i;                   /* The counter for the loop */
 char  ltr_grade[] = {      /* Letter Grades stored as an array */
  'A', 'B', 'C', 'D', 'F'
 };
+int grade_arr_size = sizeof(grades) / sizeof(int); //Get how many are in array by getting byte value array and dividing by size of int
 
+int sum(int arr[], int last) {
+  if (last <= 0)
+    return 0;
+  return (sum(arr, last-1) + arr[last-1]);
+}
+
+/* For loop to calculate grade total */
+void grade_sum_alt(int arr[]){
+  for(i=0; i<grade_arr_size; i++){
+    printf("%d ", arr[i]);
+    grade_total = grade_total + arr[i];
+  }
+}
 
 char grade_check(float grade) { /* A function to convert grade to letter grade */
   if(grade > 90.0) {
@@ -31,6 +45,8 @@ char grade_check(float grade) { /* A function to convert grade to letter grade *
 }
 
 int main() {
+  printf("%d\n", sum(grades, grade_arr_size)); /*Get sum of all*/
+
   printf("Enter Name: ");
   fgets(std_name, sizeof(std_name), stdin);
   std_name[strlen(std_name)-1] = '\0'; /* Removes newline at the end of string/char array */
@@ -38,19 +54,12 @@ int main() {
   /* strcpy(std_name, "Jim");                        // Set the kid's name */
   printf("Pulling information for student, %s...\n", std_name); // %s accepts a string into the string interpolation, %d would be for ints
 
-  /* Calculate grades */
+  /* Calculate/Print grades */
   printf("Calculating grades...\n");
-  grade_amount = sizeof(grades) / sizeof(int); //Get how many are in array by getting byte value array and dividing by size of int
-
-  for(i=0; i<grade_amount; i++){
-    printf("%d ", grades[i]);
-    grade_total = grade_total + grades[i];
-  }
   printf("\nGrade Total: %d\n", grade_total);
-  printf("Number Of Grades: %d\n", grade_amount);
+  printf("Number Of Grades: %d\n", grade_arr_size);
   grade_avg = grade_total / grade_amount;
-  /* Print Grades */
-  printf("Your Average: %f\n", grade_avg);    //Print grade average, note the string interpolation
+  printf("Your Average: %f\n", grade_avg); /* Print grade average, note the string interpolation */
   printf("Your letter grade: %c\n", grade_check(grade_avg));
   printf("Keep up the good work %s\n", std_name);
   return(0);
