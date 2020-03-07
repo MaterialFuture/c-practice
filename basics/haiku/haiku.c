77dbcc127eff9a77fad572d29b1a41e0e9a341b6
Konstantine <konstantinev@protonmail.com>
2020-03-07 11:46:20 -0600

    Started haiku script

---
+++ basics/haiku/haiku.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+char first_arr[3];
+char middle_arr[2];
+char last_arr[3];
+
+char word_arr[20];
+/* TODO: Create prefilled array of random wods, or
+ * Make HTTP request to API for randome words and generate an array */
+
+int main() {
+  int i;
+  for (i = 0; i < 2; i++) {
+    first_arr[i] = word_arr[].sample; /* Not sure what sample would be in C from Ruby */
+  }
+  for (i = 0; i < 1; i++) {
+    first_arr[i] = word_arr[i];
+  }
+
+  return 0;
+}
