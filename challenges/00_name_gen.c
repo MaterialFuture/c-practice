92623c928e7c442eebd27747c982c27b5e2dc7e7
Konstantine <konstantinev@protonmail.com>
2020-03-13 22:17:42 -0500

    updated name_gen with comments and cleanup too

--- challenges/00_name_gen.c
+++ challenges/00_name_gen.c
@@ -12,15 +12,15 @@
 char name[MAX_NUM];   /* Generated name, mostly to set the pointer */
 char f_name[MAX_NUM]; /* First name to print */
 
-int rand_num(int min_num, int max_num) { /* A function that simply creates a random number */
-  srand(time(NULL));
-  int result = 0;
-  result = (rand() % (min_num - max_num) + min_num);
-  return result;
+int rand_num(unsigned int min_num, unsigned int max_num) { /* A function that simply creates a random number */
+  srand(time(NULL));                                       /* A trick that generates it based on the computers time in seconds iirc */
+  int result = 0;                                          /* init */
+  result = (rand() % (min_num - max_num) + min_num);       /* Yet anther tick to get the value you want */
+  return result;                                           /* return value desired */
 }
 
 int main(void) {
-  int r = rand_num(0,21); /* Random number from 0...20 */
-  printf("%d\n", r);
+  unsigned int r = rand_num(0,21); /* Random number from 0...20 */
+  printf("%d\n", r);               /* Print random number */
   return 0;
 }
