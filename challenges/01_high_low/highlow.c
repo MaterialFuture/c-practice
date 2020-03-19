028227d239761e5acaad5735b5d2c77cf74c86da
Konstantine <konstantinev@protonmail.com>
2020-03-18 19:11:07 -0500

    move cointoss and created simple high/low game

--- challenges/01_high_low/highlow.c
+++ challenges/01_high_low/highlow.c
@@ -1,15 +1,12 @@
-/***********************************************************/
-/* Title: Heads or Tails                                   */
-/* Desc:  A simple implementation of heads or tails        */
-/* Bonus: Work on higher or lower implementation, creating */
-/***********************************************************/
+/******************************************************************/
+/* Title: Higher and Lower game in C                              */
+/* Desc:  Similar to cointoss but guessing a number is higher or  */
+/******************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <time.h>
 
-char user_input[10];
-char cpu_coin; /* Storing the coin value as a single Char */
+int user_input;
 
 const int rand_num(int min_num, int max_num) {       /* A function that simply creates a random number */
   srand(time(NULL));                                 /* A trick that generates it based on the computers time in seconds iirc */
@@ -18,28 +15,31 @@ const int rand_num(int min_num, int max_num) {       /* A function that simply c
   return result;                                     /* return value desired */
 }
 
-char coin_arr[2][2] = { /* An array of 2 objects with length of 2 */
-  "h", "t"              /* Heads and Tails */
-};
-
 int main() {
-  printf("Let's Play Heads or Tails\n");         /* Print the name of the game */
+  printf("Let's Play Higher Or Lower\n");        /* Print the name of the game */
+  printf("The Number is 1-10\n");                /* Give user some info */
+  const int r = rand_num(0,10);                  /* Random Num */
+  /* For Hard mode move the above into the loop to have it change each time lol */
+
   while(1){                                      /* Initiate Game */
-   printf("h or t? ");                           /* Prompt user */
-   fgets(user_input, sizeof(user_input), stdin); /* Get User Input */
-   user_input[strlen(user_input)-1] = '\0';      /* Trim off that last bit */
+   printf("Type Number: ");                      /* Prompt user */
+   scanf("%d", &user_input);                     /* Trim off that last bit */
+   printf("Your Guess:\t%d\n", user_input);      /* Prints your guess */
 
-   const int r = rand_num(0,2);                  /* Random Num to get h or t */
-   cpu_coin = coin_arr[r][0];                    /* Get random object in array,  */
-   printf("CPU:\t\t%c\n", cpu_coin);             /* Print what the "CPU" has got on their coin */
+   /* Not sure how to sanitize input of user here */
 
-   if (user_input[0] != cpu_coin) {              /* If user input matches or doesn't the SPUs then print something */
-     printf("No Match\n");
+   if (user_input == 0){
+     printf("No zeros or letters pls...");
+   } else if (user_input < r) {                         /* If Guess is lower */
+     printf("Higher\n");                         /* Print to guess a hgiher */
+   } else if (user_input > r) {
+     printf("Lower\n");
    } else {
-     printf("You Won!\n");
+     printf("You Won!\n");                       /* If matches */
+     printf("CPU:\t\t%d\n", r);                  /* What the CPU Guesses */
      break;                                      /* If you beat the CPU then exit the game */
    }
-   printf("Let's Try Again...\n");               /* Print something to let the user know that the game (loop) is restarting */
+   printf("Restarting...\n");               /* Print something to let the user know that the game (loop) is restarting */
   }
   return 0;
 }
